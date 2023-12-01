b48fff149e3b42bfc785451fe8b1440952b7edd0
placeholder <invalid@example.com>
2023-12-01 15:11:37 +0100

    fix dict

--- doom/.config/doom/init.el
+++ doom/.config/doom/init.el
@@ -81,7 +81,7 @@
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
-       (spell +aspell +flyspell) ; tasing you for misspelling mispelling
+       (spell +flyspell) ; tasing you for misspelling mispelling
        ;;grammar           ; tasing grammar mistake every you make
 
        :tools
