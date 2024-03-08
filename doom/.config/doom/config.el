00ece84cd042337f87e61fa6beb3b93e9fa84ccc
placeholder <invalid@example.com>
2024-03-08 23:43:34 +0100

    emacs

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -90,10 +90,40 @@
       '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
         "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
 
+(setq lsp-clients-clangd-args '("-j=3"
+				"--background-index"
+				"--clang-tidy"
+				"--completion-style=detailed"
+				"--header-insertion=never"
+				"--header-insertion-decorators=0"))
+(after! lsp-clangd (set-lsp-priority! 'clangd 2))
+
+(require 'srefactor)
+
 (require 'dap-cpptools)
 
 ;;(add-hook 'after-init-hook #'global-flycheck-mode)
 
 ;;(setq org-latex-listings 't)
+
+
+;; C / C++ code generation
+(require 'srefactor)
+(require 'srefactor-lisp)
+(define-key c-mode-map (kbd "M-RET") 'srefactor-refactor-at-point)
+(define-key c++-mode-map (kbd "M-RET") 'srefactor-refactor-at-point)
+(global-set-key (kbd "M-RET o") 'srefactor-lisp-one-line)
+(global-set-key (kbd "M-RET m") 'srefactor-lisp-format-sexp)
+(global-set-key (kbd "M-RET d") 'srefactor-lisp-format-defun)
+(global-set-key (kbd "M-RET b") 'srefactor-lisp-format-buffer)
+
+
+
+(setq lsp-ui-sideline-enable t)
+(setq global-whitespace-mode nil)
+
+(after! lsp-ui
+  (setq lsp-ui-doc-enable t))
+
 
 (require 'ox-man)
--- doom/.config/doom/init.el
+++ doom/.config/doom/init.el
@@ -95,10 +95,10 @@
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
-       lookup              ; navigate your code and its documentation
+       (lookup +dictionary)              ; navigate your code and its documentation
        lsp               ; M-x vscode
        magit             ; a git porcelain for Emacs
-       ;;make              ; run make tasks from Emacs
+       make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
        ;;pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
