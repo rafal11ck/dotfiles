e6b723849b9cab9e2121488cd6ec65b2a46b731a
placeholder <invalid@example.com>
2024-03-31 15:30:20 +0200

    wallpaper with swww

--- scripts/.config/scripts/autorun.sh
+++ scripts/.config/scripts/autorun.sh
@@ -8,4 +8,5 @@ setsid jellyfin-mpv-shim 2>&1 > /dev/null || notify-send 'jellyfin-mpv-shim fail
 #polkit
 setsid /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 2>&1 > /dev/null &
 
+
 notify-send -t 15000 'autorun done'
