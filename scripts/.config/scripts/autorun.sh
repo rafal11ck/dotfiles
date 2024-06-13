c667e9c9e791749ed2c31daa57d60c631fe4efaa
Your Name <you@example.com>
2024-06-13 08:09:16 +0200

    config

--- scripts/.config/scripts/autorun.sh
+++ scripts/.config/scripts/autorun.sh
@@ -8,4 +8,7 @@ setsid jellyfin-mpv-shim 2>&1 > /dev/null || notify-send 'jellyfin-mpv-shim fail
 #polkit
 setsid /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 2>&1 > /dev/null &
 
+
+setsid play-with-mpv 2>&1 > /dev/null &
+
 notify-send -t 30000 'autorun done'
