cb988919056826edf9363c9472592e87ec82f022
Your Name <you@example.com>
2024-07-29 22:23:30 +0200

    break stuff

--- scripts/.config/scripts/nightmode.sh
+++ scripts/.config/scripts/nightmode.sh
@@ -1,7 +1,33 @@
 #!/bin/sh
 
+
 if [ -z "$(pidof gammastep)" ]; then
-    gammastep -O 3700
+    text="";
+    tooltip="activate nightmode"
+    procentage="100"
+    gammastep -O 3700 > /dev/null 2>&1 &
 else
+    text="";
+    tooltip="deactivate nightmode"
+    class="active"
+    procentage="0"
     kill -9 "$(pidof gammastep)"
 fi
+
+
+echo "$text"
+echo "$tooltip"
+echo "$class"
+echo "$procentage"
+
+
+exit
+
+result="$( jq -n \
+    --arg text "$text" \
+    --arg tooltip "$tooltip" \
+    --arg class "$class" \
+    --arg procentage "$procentage" \
+    '{"text": $text, "tooltip": $tooltip, "class": $class , "procentage": $procentage }' )"
+
+echo "$result"
