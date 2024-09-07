2336491409a05e43bf8a10b36b3124089f97c20a
Your Name <you@example.com>
2024-09-07 11:39:12 +0200

    nix

--- doom/.config/doom/init.el
+++ doom/.config/doom/init.el
@@ -150,7 +150,7 @@
        (lua +lsp +tree-sitter)               ; one-based indices? one-based indices
        (markdown +grip)          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
-       ;;nix               ; I hereby declare "nix geht mehr!"
+       (nix +lsp +tree-sitter)               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org +present +hugo +dragndrop +pretty +gnuplot)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
